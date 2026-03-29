e9de8200a7e9cef9b385a652003317bc320bebd6
Ivan Busygin <busygin_02@list.ru>
2026-03-29 13:15:46 +0200

    Cache cart items

--- market-app/src/main/java/ru/yandex/practicum/market/redis/ItemCacheService.java
+++ market-app/src/main/java/ru/yandex/practicum/market/redis/ItemCacheService.java
@@ -2,11 +2,15 @@ package ru.yandex.practicum.market.redis;
 
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import ru.yandex.practicum.market.persistence.entity.ItemR2dbcEntity;
 import ru.yandex.practicum.market.persistence.repository.ItemR2dbcRepository;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @Service
 public class ItemCacheService {
@@ -30,6 +34,38 @@ public class ItemCacheService {
                 itemRedisTemplate.opsForValue().set(key, item, TTL).thenReturn(item)
             )
         ));
+    }
+
+    public Mono<List<ItemR2dbcEntity>> getByIds(List<Long> ids) {
+        if (ids.isEmpty()) return Mono.just(List.of());
+
+        List<String> keys = ids.stream().map(this::key).toList();
+        return itemRedisTemplate.opsForValue().multiGet(keys).flatMap(cachedItems -> {
+            List<Long> missingIds = new ArrayList<>();
+            List<ItemR2dbcEntity> items = new ArrayList<>();
+
+            for (int i = 0; i < ids.size(); i++) {
+                ItemR2dbcEntity cachedItem = cachedItems.get(i);
+                if (cachedItem == null) {
+                    missingIds.add(ids.get(i));
+                } else {
+                    items.add(cachedItem);
+                }
+            }
+
+            if (missingIds.isEmpty()) return Mono.just(items);
+
+            return itemRepository.findAllById(missingIds).collectList().flatMap(dbItems -> {
+                items.addAll(dbItems);
+
+                Mono<Void> cacheItems = Flux.fromIterable(dbItems)
+                    .filter(item -> Objects.nonNull(item.getId()))
+                    .flatMap(item -> itemRedisTemplate.opsForValue().set(key(item.getId()), item, TTL))
+                    .then();
+
+                return cacheItems.thenReturn(items);
+            });
+        });
     }
 
     private String key(long id) {
